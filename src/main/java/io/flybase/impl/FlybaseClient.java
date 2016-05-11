2ad40c48fa1a40d969ef10597f558a2071d5d4f0
alexandre.dantas@yahoo.com.br <alexandre.dantas@yahoo.com.br>
2016-05-11 13:45:12 -0300

    Improving API usage

--- src/main/java/io/flybase/impl/FlybaseClient.java
+++ src/main/java/io/flybase/impl/FlybaseClient.java
@@ -5,16 +5,31 @@
  */
 package io.flybase.impl;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.owlike.genson.GenericType;
 import com.owlike.genson.Genson;
 import com.owlike.genson.GensonBuilder;
 import io.flybase.Collection;
+import io.flybase.Document;
 import io.flybase.Flybase;
 import io.flybase.exceptions.CollectionsRetrievingException;
+import io.flybase.exceptions.DeletingDocumentException;
+import io.flybase.exceptions.WritingDocumentException;
+import io.flybase.query.QueryBuilder;
+import io.flybase.query.impl.SimpleQueryBuilder;
+import io.flybase.query.impl.validators.AtLeastOne;
+import io.flybase.query.impl.validators.NoOne;
+import io.flybase.query.impl.validators.OnlyOne;
+import io.flybase.query.types.ParameterType;
 import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
+import org.json.JSONObject;
 
 /**
  *
@@ -50,7 +65,30 @@ public class FlybaseClient implements Flybase {
         }
     }
 
-    FlybaseClient(String apiKey, String slug) {
+    public static class FlybaseClientBuilder {
+
+        private String apiKey;
+        private String slug;
+
+        private FlybaseClientBuilder() {
+        }
+
+        public FlybaseClientBuilder apiKey(String api) {
+            this.apiKey = api;
+            return this;
+        }
+
+        public FlybaseClientBuilder slug(String slug) {
+            this.slug = slug;
+            return this;
+        }
+
+        public Flybase build() {
+            return new FlybaseClient(this.apiKey, this.slug);
+        }
+    }
+
+    private FlybaseClient(String apiKey, String slug) {
         this.requestParameters = new FlybaseRequestParameters(apiKey, slug);
         this.genson = new GensonBuilder()
                 .failOnMissingProperty(false)
@@ -58,16 +96,21 @@ public class FlybaseClient implements Flybase {
                 .create();
     }
 
+    public static FlybaseClientBuilder builder(){
+        return new FlybaseClientBuilder();
+    }
+
     @Override
+
     public List<Collection> collections() {
         try {
             return this.genson.deserialize(Unirest.get(getCollectionsUrl())
                     .header(API_KEY_HEADER_NAME, this.requestParameters.getApiKey())
                     .asJson().getBody().getArray().toString(),
                     new GenericType<List<String>>() {
-            }).parallelStream()
+                    }).parallelStream()
                     .map(name -> (Collection) new CollectionImpl(getCollectionUrl(name),
-                            this.requestParameters))
+                                    this.requestParameters))
                     .collect(Collectors.toList());
         } catch (UnirestException ex) {
             throw new CollectionsRetrievingException(ex);
@@ -87,5 +130,91 @@ public class FlybaseClient implements Flybase {
     private String getCollectionUrl(String collectionName) {
         return String.format(COLLECTION_URL, new Object[]{this.requestParameters.getSlug(),
             collectionName});
+    }
+
+    private class CollectionImpl implements Collection {
+
+        private final Logger LOGGER = Logger.getLogger(CollectionImpl.class.
+                getName());
+
+        private final String collectionUrl;
+        private final FlybaseClient.FlybaseRequestParameters requestParameters;
+
+        private CollectionImpl(String url, FlybaseClient.FlybaseRequestParameters requestParamters) {
+            this.collectionUrl = url;
+            this.requestParameters = requestParamters;
+        }
+
+        @Override
+        public Optional<Document> retrieve(String id) {
+            try {
+                JSONObject response = Unirest.get(collectionUrl + "/" + id)
+                        .header(FlybaseClient.API_KEY_HEADER_NAME, this.requestParameters.getApiKey())
+                        .asJson()
+                        .getBody()
+                        .getObject();
+
+                if (response.length() > 0) {
+                    return Optional.of(Document.raw(response));
+                } else {
+                    return Optional.empty();
+                }
+            } catch (Exception e) {
+                LOGGER.log(Level.WARNING, "Error retrieving document by id", e);
+                return Optional.empty();
+            }
+        }
+
+        @Override
+        public Document save(Document doc) {
+            try {
+                HttpResponse<JsonNode> response = Unirest.post(this.collectionUrl)
+                        .header(FlybaseClient.API_KEY_HEADER_NAME, this.requestParameters.getApiKey())
+                        .body(doc.getRaw())
+                        .asJson();
+
+                if (response.getStatus() > 201) {
+                    throw new WritingDocumentException(
+                            String.format("Error writing document. Status: %d", response.getStatus()));
+
+                } else {
+                    return Document.raw(response.getBody().getObject());
+                }
+            } catch (UnirestException e) {
+                throw new WritingDocumentException(e);
+            }
+        }
+
+        @Override
+        public void delete(String id) {
+            try {
+                HttpResponse<JsonNode> response = Unirest.delete(collectionUrl + "/" + id)
+                        .header(FlybaseClient.API_KEY_HEADER_NAME, this.requestParameters.getApiKey())
+                        .asJson();
+
+                if (response.getStatus() != 200) {
+                    throw new DeletingDocumentException(String.format("Error deleting document!\nError:%s",
+                            response.getBody().getObject().toString()));
+                }
+            } catch (UnirestException e) {
+                LOGGER.log(Level.WARNING, "Error deleting document", e);
+                throw new DeletingDocumentException(e);
+            }
+        }
+
+        @Override
+        public QueryBuilder query() {
+            return new SimpleQueryBuilder(Unirest.get(this.collectionUrl)
+                    .header(FlybaseClient.API_KEY_HEADER_NAME, this.requestParameters.getApiKey()),
+                    new NoOne(ParameterType.BODY));
+        }
+
+        @Override
+        public QueryBuilder batchUpdate() {
+            return new SimpleQueryBuilder(Unirest.put(this.collectionUrl)
+                    .header(FlybaseClient.API_KEY_HEADER_NAME, this.requestParameters.getApiKey()),
+                    new AtLeastOne(ParameterType.QUERY),
+                    new OnlyOne(ParameterType.BODY));
+        }
     }
 }
